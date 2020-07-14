8d1720f4b30de871a1c4c35c74f3f97b55805f3a
David <davidacosta25081@yahoo.com>
2020-07-14 13:13:12 -0400

    Ordering Gym List button

--- client/src/components/GymList.js
+++ client/src/components/GymList.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import Gym from './Gym';
 
-const GymList = (props) => {
+import Gym from './Gym';
+import React, { Component } from 'react';
+  
+class GymList extends React.Component {
   
   
-  return (
-    <div className="GymListcontainer">
-      <div className="row">
-        {props.gyms.map(gym =>  <Gym key={gym.id} gym={gym}/>)}       
-      </div>
-   </div>
-   )
+  state = { gyms: [], toggle: true }
+  
+  orderedList = () => {
+    this.setState({ gyms: 
+    this.props.gyms.sort(function(a, b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+    }) , toggle: false })
+  }
+  
+  
+    render () {
+      
+      return (
+        this.state.toggle ?  
+
+        <div className="GymListcontainer">
+          <div className="row">
+            {this.props.gyms.map(gym =>  <Gym key={gym.id} gym={gym}/>)}       
+          </div>
+          <div><button className='orderingButton' onClick={this.orderedList}>SORT GYMS</button></div>
+        </div> :
+      
+        <div className="GymListcontainer">
+          <div className="row">
+            {this.state.gyms.map(gym =>  <Gym key={gym.id} gym={gym}/>)}      
+          </div>
+        </div>
+      )
+    }
 }
 
 export default GymList;
