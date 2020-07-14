f15b0d4e3a86ec56643c79bfcecfc26dc72204e4
David <davidacosta25081@yahoo.com>
2020-07-13 23:17:46 -0400

    organizing divs on comments section

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -8,25 +8,13 @@ const Reviews = (props) => {
     
   return (
     <div>
-    {props.reviews.filter(review => review.gym.id === props.gymId) 
-    .map(review =>
+      {props.reviews.filter(review => review.gym.id === props.gymId) 
+      .map(review =>
       <div className="card-body" key={review.id}>
       <div className="card-title"  key={review.id}> <h2><b>{review.title}</b></h2><br></br> <h3>{review.description}</h3><br></br><hr/></div>
-    
- 
-  </div> )}   
-    
-  </div>
-  
-  
-  
-  
-  
-  
-  
-  
- 
-)
+      </div> )}   
+    </div>
+  )
 
 }
 
