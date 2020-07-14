4c4f155948b25f962df282a996cd0d9a0f73f74e
David <davidacosta25081@yahoo.com>
2020-07-13 23:13:15 -0400

    adding unique key to reviews

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -7,29 +7,26 @@ const Reviews = (props) => {
     
     
   return (
-      <div>
-      {props.reviews.filter(review => review.gym.id === props.gymId) 
-      .map(review =>
-      
-        <div class="card-body">
-      
-        
-      <div className="card-title"  key={review.id}> <h2><b>{review.title}</b>  </h2> </div>
-      <div className="card-text"> <h3> {review.description}</h3> <hr />    </div>
-   
-    </div> )}   
-      
-    </div>
+    <div>
+    {props.reviews.filter(review => review.gym.id === props.gymId) 
+    .map(review =>
+      <div className="card-body" key={review.id}>
+      <div className="card-title"  key={review.id}> <h2><b>{review.title}</b></h2><br></br> <h3>{review.description}</h3><br></br><hr/></div>
     
+ 
+  </div> )}   
     
-    
-    
-    
-    
-    
-    
-   
-  )
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+)
 
 }
 
